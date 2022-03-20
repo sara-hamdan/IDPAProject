901220059f3c125af62bd57f52ad3b41af6c07ac
sara-hamdan <sarahhamdan53@gmail.com>
2022-03-20 21:45:50 +0200

    Initial Commit

--- src/main/java/com/example/idpaproject1/other/NiermanJagadishAlgorithm.java
+++ src/main/java/com/example/idpaproject1/other/NiermanJagadishAlgorithm.java
@@ -1,6 +1,8 @@
 package com.example.idpaproject1.other;
 
 import org.w3c.dom.*;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
 import org.w3c.dom.traversal.TreeWalker;
 
 import java.lang.reflect.Array;
@@ -11,6 +13,8 @@ import static java.lang.Math.min;
 
 public class NiermanJagadishAlgorithm {
 
+    Document A;
+    Document B;
 
     String inputFile;
 
@@ -71,96 +75,156 @@ public class NiermanJagadishAlgorithm {
 
     }
 
-    public void NiermanJagadishExecute(Document A, Document B) {
+    public int NiermanJagadishExecute(Node nodeA, Node nodeB, Document documentA, Document documentB) {
 
-        int heightA = getHeightDocument(A);
-        int heightB = getHeightDocument(B);
+        Node parentNodeA = nodeA;
+        Node parentNodeB = nodeB;
+        int degreeA = getDegree(parentNodeA, documentA);
+        int degreeB = getDegree(parentNodeB, documentB);
 
-        int[][] editDistanceMatrix = new int[heightA][heightB];
-        editDistanceMatrix[0][0] = costUpdate(A.getElementById("OrdinaryIssuePage"), B.getElementById("OrdinaryIssuePage"));
+        int[][] editDistanceMatrix = new int[degreeA+1][degreeB+1];
+        editDistanceMatrix[0][0] = costUpdate(parentNodeA, parentNodeB);
 
-        for (int i = 1; i <= heightA; i++) {
 
-            // editDistanceMatrix[i][0] = editDistanceMatrix[i-1][0]
+        ArrayList<Node> levelSubtreesArrayA = getLevelSubtrees(parentNodeA, documentA);
+        ArrayList<Node> levelSubtreesArrayB = getLevelSubtrees(parentNodeB, documentB);
+        getLevelSubtrees(parentNodeB, documentB);
+        for (int i = 1; i <= degreeA; i++) {
+
+            for (int j = 0; j < levelSubtreesArrayA.size() ; j++) {
+                System.out.println(levelSubtreesArrayA.get(j));
+            }
+            editDistanceMatrix[i][0] = editDistanceMatrix[i-1][0] + getCost(levelSubtreesArrayA.get(i-1), documentB);
 
         }
 
+        for (int j = 1; j <= degreeB; j++) {
+
+
+            editDistanceMatrix[0][j] = editDistanceMatrix[0][j-1] + getCost(levelSubtreesArrayB.get(j-1), documentA);
+        }
+
+        for (int i = 1; i <= degreeA; i++) {
+            for (int j = 1; j <= degreeB; j++) {
+                editDistanceMatrix[i][j] = min(
+                        min(editDistanceMatrix[i-1][j-1] + NiermanJagadishExecute(levelSubtreesArrayA.get(i-1), levelSubtreesArrayB.get(j-1), documentA, documentB),
+                                editDistanceMatrix[i-1][j]+ getCost(levelSubtreesArrayA.get(i-1), documentB)),
+                                editDistanceMatrix[i][j-1] + getCost(levelSubtreesArrayB.get(j-1), documentA));
+            }
+        }
+
+
+        return editDistanceMatrix[degreeA][degreeB];
 
     }
 
 
     public int costUpdate(Node nodeA, Node nodeB) {
 
-        if (nodeA == nodeB) return 0;
+        if (String.valueOf(nodeA).equals(String.valueOf(nodeB))) return 0;
 
-        else
-            return 1;
-
+        else return 1;
 
     }
-
 
 
     public boolean isContainedIn(Node testNode, ArrayList<Node[]> subtreesListDoc1) {
         boolean isContained = true;
 
-            NodeList nodeList = testNode.getChildNodes();
-            Node[] arrayOfNodes = new Node[nodeList.getLength() + 1];
-            arrayOfNodes[0] = testNode;
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                arrayOfNodes[i + 1] = nodeList.item(i);
-
-            }
-
-
-            for (int i = 0; i < subtreesListDoc1.size(); i++) {
-
-                  for (int j = 0; j< min(subtreesListDoc1.get(i).length, arrayOfNodes.length); j++) {
-
-                      if (!String.valueOf(arrayOfNodes[j]).equals(String.valueOf(subtreesListDoc1.get(i)[j]))) {
-                          isContained = false;
-                          break;
-                      }
-                  }
-
-                  isContained = true;
-
-            }
-
-            return isContained;
+        NodeList nodeList = testNode.getChildNodes();
+        Node[] arrayOfNodes = new Node[nodeList.getLength() + 1];
+        arrayOfNodes[0] = testNode;
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            arrayOfNodes[i + 1] = nodeList.item(i);
 
         }
 
 
+        for (int i = 0; i < subtreesListDoc1.size(); i++) {
+
+            for (int j = 0; j < min(subtreesListDoc1.get(i).length, arrayOfNodes.length); j++) {
+
+                if (!String.valueOf(arrayOfNodes[j]).equals(String.valueOf(subtreesListDoc1.get(i)[j]))) {
+                    isContained = false;
+                    break;
+                }
+            }
 
 
+        }
+
+        return isContained;
+
+    }
 
 
+    public int getCost(Node node, Document document) {
 
 
+        if (!node.hasChildNodes()) return 1;
 
-    public void getSubtrees(Document document, ArrayList<Node[]> subtreesList, TreeWalker treeWalker) {
+        ArrayList<Node[]> arrayList = new ArrayList<>();
+        DocumentTraversal traversal = (DocumentTraversal) document;
+        TreeWalker treeWalker = traversal.createTreeWalker(document.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
+        arrayList = getSubtrees(document, arrayList, treeWalker);
+        if (isContainedIn(node, arrayList)) return 1;
+        else {
+
+            int cost = 1 + node.getChildNodes().getLength();
+
+            return cost;
+        }
+    }
+
+
+    public ArrayList<Node[]> getSubtrees(Document document, ArrayList<Node[]> subtreesList, TreeWalker treeWalker) {
 
         Node parent = treeWalker.getCurrentNode();
 
         for (Node n = treeWalker.firstChild(); n != null; n = treeWalker.nextSibling()) {
 
             NodeList nodeList = n.getChildNodes();
-            Node[] arrayOfNodes = new Node[nodeList.getLength()+1];
+            Node[] arrayOfNodes = new Node[nodeList.getLength() + 1];
             arrayOfNodes[0] = n;
             for (int i = 0; i < nodeList.getLength(); i++) {
-                arrayOfNodes[i+1] = nodeList.item(i);
+                arrayOfNodes[i + 1] = nodeList.item(i);
             }
-             subtreesList.add(arrayOfNodes);
-           getSubtrees(document, subtreesList, treeWalker);
+            subtreesList.add(arrayOfNodes);
+            getSubtrees(document, subtreesList, treeWalker);
         }
         treeWalker.setCurrentNode(parent);
 
 
+        return subtreesList;
+    }
 
+    public int getDegree(Node node, Document document) {
+
+        if (node == null) return 1;
+        DocumentTraversal traversal = (DocumentTraversal) document;
+        TreeWalker treeWalker = traversal.createTreeWalker(document.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
+        treeWalker.setCurrentNode(node);
+        int countLevelTrees = 0;
+        for (Node n = treeWalker.firstChild(); n != null; n = treeWalker.nextSibling()) {
+            countLevelTrees++;
+        }
+        return countLevelTrees;
+    }
+
+    public ArrayList<Node> getLevelSubtrees(Node node, Document document) {
+
+        DocumentTraversal traversal = (DocumentTraversal) document;
+        TreeWalker treeWalker = traversal.createTreeWalker(document.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
+        treeWalker.setCurrentNode(node);
+        ArrayList<Node> levelSubtrees = new ArrayList<>();
+
+        for (Node n = treeWalker.firstChild(); n != null; n = treeWalker.nextSibling()) {
+            levelSubtrees.add(n);
+        }
+
+        return levelSubtrees;
     }
 }
-
 
 
 
--- src/main/java/com/example/idpaproject1/other/TestParser.java
+++ src/main/java/com/example/idpaproject1/other/TestParser.java
@@ -17,17 +17,23 @@ public class TestParser {
     public static void main(String[] args) throws ParserConfigurationException {
 
         NiermanJagadishAlgorithm nandj = new NiermanJagadishAlgorithm();
-        Document doc = nandj.parseXML("src/testFile.xml");
+        Document doc = nandj.parseXML("src/SampleDoc1 (original) Refined.xml");
 
 
         System.out.println();
         DocumentTraversal traversal = (DocumentTraversal) doc;
         TreeWalker treeWalker = traversal.createTreeWalker(doc.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
-        Document doc2 = nandj.parseXML("src/testFile2.xml");
+        Document doc2 = nandj.parseXML("src/SampleDoc1 (original) V1 Refined.xml");
 
-        System.out.println();
+        DocumentTraversal traversal2 = (DocumentTraversal) doc2;
+        TreeWalker treeWalker2 = traversal.createTreeWalker(doc2.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
+
+
+
+
+                System.out.println();
         ArrayList<Node[]> arrayList = new ArrayList<>();
-        nandj.getSubtrees(doc, arrayList, treeWalker);
+                arrayList = nandj.getSubtrees(doc, arrayList, treeWalker);
         int count = 0;
         for (int i = 0; i < arrayList.size(); i++) {
             Node[] array = arrayList.get(i);
@@ -43,10 +49,21 @@ public class TestParser {
             count++;
         }
         System.out.println(count);
-
         System.out.println(nandj.isContainedIn(doc2.getDocumentElement().getChildNodes().item(1), arrayList));
+        System.out.println(nandj.getCost(doc2.getDocumentElement().getChildNodes().item(1), doc ));
+        System.out.println(nandj.getDegree(doc2.getDocumentElement(), doc2));
+
+
+        ArrayList<Node> nodes = nandj.getLevelSubtrees(doc.getDocumentElement(), doc);
+        for (int i = 0; i < nodes.size(); i++) {
+            System.out.println(nodes.get(i));
+        }
+
+        System.out.println(nandj.NiermanJagadishExecute(doc.getDocumentElement(), doc2.getDocumentElement(), doc, doc2));
+
 
     }
+
 
 
 
